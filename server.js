910067342e002fd477641b345a5a4f6808c68c28
katherine <kmarmitage@gmail.com>
2019-06-30 21:01:04 -0700

    corrected server.js

--- server.js
+++ server.js
@@ -7,23 +7,23 @@ var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Parse application body as JSON
+// Parsing middleware (parses json objects and url encoded data)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
+// Require handlebars
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+// Import routes 
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
+// Start the server
 app.listen(PORT, function () {
-    // Log (server-side) when our server has started
+    //server-side message to show that the server has started
     console.log("Server listening on: http://localhost:" + PORT);
 });
