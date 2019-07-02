d9f33f98a0544e2bb15ffe73d98ff1a29424213a
katherine <kmarmitage@gmail.com>
2019-07-01 21:35:37 -0700

    added image and styling

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var methodOverride = require("method-override");
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 
 // Parsing middleware (parses json objects and url encoded data)
 app.use(express.urlencoded({ extended: true }));
